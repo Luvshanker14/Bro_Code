c4b5f805bba9d75deaec53475c6d14838ccaae25
Priyanshu Mishra <mishrapriyanshu2912@gmail.com>
2024-06-11 20:36:47 +0530

    Adding initial Backend code

---
+++ backend/controllers/userCtrl.js
@@ -0,0 +1,83 @@
+const Users=require('../models/userModel');
+const jwt=require("jsonwebtoken");
+const bcrypt=require("bcrypt");
+const z = require('zod');
+
+const emailValidate=z.string().email().refine((email)=>{
+  return email.endsWith("@iitdh.ac.in");
+});
+
+const passwordValidate=z.string().min(8);
+
+const userCtrl={
+  register:async(req,res)=>{
+    try {
+      console.log("i am in register controller");
+      const {name,email,password}=req.body;
+
+      const eValidation=emailValidate.safeParse(email);
+      const passValidation=passwordValidate.safeParse(password);
+      if(!eValidation.success)
+        {
+          console.log("i am in email validation not validated");
+          // res.send("Use your College EmailId to register");
+          return res.status(400).json({ error: "Use your College EmailId to register" });
+        }
+        if(!passValidation.success)
+          {
+            console.log("i am in password validation not validated")
+            // res.send("Password should have atleast 8 characters");
+            return res.status(400).json({ error: "Password shoul have at least 8 characters" });
+          }
+
+      const user=await Users.findOne({email});
+      if(user)
+        {
+          
+          return res.status(400).json({msg:"Email already registered"});
+        }
+
+        //hashing password
+        const passwordHash=await bcrypt.hash(password,10);
+
+        const newUser= new Users({
+          name,email,password
+        });
+        await newUser.save();
+
+       res.status(200).json({msg:"User registered"});
+
+
+
+
+    } catch (error) {
+      return res.json(500).json({msg:error.message});
+    }
+  },
+  login:async(req,res)=>{
+
+    try{
+      const {email,password}=req.body;
+      const user=await Users.findOne({email,password});
+      console.log("hey");
+      console.log(email);
+      console.log(password);
+      if(!user)
+      {
+        return res.status(404).send("User not Found");
+      }
+      res.status(200).json({success:true,user,});
+      console.log("hey2");
+      }catch(error)
+    {
+      res.status(400).json({
+        success:false,
+        error,
+      });
+      console.log(error);
+  
+    }
+
+  }
+}
+module.exports=userCtrl;
---
+++ backend/routes/userRouter.js
@@ -0,0 +1,8 @@
+const userCtrl = require("../controllers/userCtrl");
+
+const router=require("express").Router();
+
+router.post('/register',userCtrl.register);
+router.post('/login',userCtrl.login);
+
+module.exports=router;
