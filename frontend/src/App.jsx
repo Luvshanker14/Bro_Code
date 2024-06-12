c5f44004082239bcaf8a73353e6ad51c01629db0
Vishal Kumar <vishalkumar1981v@gmail.com>
2024-06-12 13:05:18 +0530

    Added Account page, changes in index.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
-import { FaHeart,FaRegHeart } from 'react-icons/fa';
-import image from './assets/react.svg';
-
+import image from "./assets/react.svg";
+import Status from "./pages/Status";
+import Account from "./pages/Account";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import HomeIcon from '@mui/icons-material/Dashboard';
+import AccountIcon from '@mui/icons-material/Person';
+import BooksIcon from '@mui/icons-material/AutoStories';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import SearchIcon from '@mui/icons-material/Search';
 
 function App() {
-  const [selected, setSelected] = useState('Dashboard');
+  const [selected, setSelected] = useState('Home');
 
   return (
     <div style={{ backgroundColor: 'white', display: 'flex' }}>
@@ -12,25 +20,25 @@ function App() {
       <MainContent selected={selected} />
     </div>
   );
-
 }
+
 function Navbar({ selected, setSelected }) {
   const menuItems = [
-    { name: 'Dashboard', icon: DashboardIcon },
+    { name: 'Home', icon: HomeIcon },
     { name: 'Books', icon: BooksIcon },
     { name: 'Status', icon: StatusIcon },
     { name: 'Account', icon: AccountIcon },
   ];
 
   return (
-    <div className="flex h-screen w-16 flex-col justify-between border-e bg-white">
+    <div className="navbar-container">
       <div>
-        <div className="inline-flex size-16 items-center justify-center py-4">
-          <span className="grid size-10 place-content-center rounded-lg bg-gray-100 text-xs text-gray-600">
+        <div className="navbar-logo">
+          <span>
             Logo
           </span>
         </div>
-        <div className="border-t border-gray-100">
+        <div className="navbar-divider">
           <div className="px-2">
             <div className="py-4">
               {menuItems.map((item) => (
@@ -46,11 +54,11 @@ function Navbar({ selected, setSelected }) {
           </div>
         </div>
       </div>
-      <div className="border-t border-gray-100 bg-white p-2 logout-button">
+      <div className="logout-button">
         <form action="#">
           <button
             type="submit"
-            className="group relative flex w-full justify-center rounded-lg px-2 py-1.5 text-sm text-gray-500 hover:bg-gray-50 hover:text-gray-700"
+            className="logout-button"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -66,7 +74,7 @@ function Navbar({ selected, setSelected }) {
                 d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
               />
             </svg>
-            <span className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
+            <span>
               Logout
             </span>
           </button>
@@ -80,15 +88,14 @@ function MenuItem({ name, icon: Icon, selected, setSelected }) {
   const isSelected = selected === name;
 
   return (
-    <div className="py-4">
+    <div className="menu-item">
       <a
         href="#"
         onClick={() => setSelected(name)}
-        className={`group relative flex justify-center rounded px-2 py-1.5 ${isSelected ? 'bg-blue-50 text-blue-700' : 'text-gray-500 hover:bg-gray-50 hover:text-gray-700'
-          }`}
+        className={`menu-link ${isSelected ? 'selected' : ''}`}
       >
-        <Icon className={`size-5 opacity-75 ${isSelected ? 'fill-black' : ''}`} />
-        <span className="invisible absolute start-full top-1/2 ms-4 -translate-y-1/2 rounded bg-gray-900 px-2 py-1.5 text-xs font-medium text-white group-hover:visible">
+        <Icon className={`menu-link-icon ${isSelected ? 'selected' : ''}`}  />
+        <span>
           {name}
         </span>
       </a>
@@ -101,7 +108,7 @@ function MainContent({ selected }) {
     <div className="flex-1 p-4">
       {/* Render different components based on the selected menu */}
       {selected === 'Books' && <Books />}
-      {selected === 'Dashboard' && <Dashboard />}
+      {selected === 'Home' && <Home />}
       {selected === 'Account' && <Account />}
       {selected === 'Status' && <Status />}
     </div>
@@ -109,28 +116,37 @@ function MainContent({ selected }) {
 }
 
 function Books() {
-  const books = [
-    { title: 'Amadeus: A Play', image: image},
-    { title: 'Annotated Alice: ...', image: image},
-    { title: 'Applied Numeric.', image: image },
-    { title: 'Artaud Anthology', image: image },
-    { title: 'Asterios Polyp', image: image },
-    { title: 'Batman: Year One', image: image },
-    { title: 'The Great Gatsby', image: image },
-    { title: 'To Kill a Mockingbird', image: image },
-    { title: '1984', image: image },
-    { title: 'Moby-Dick', image: image },
+  const [books, setBooks] = useState([
+    { title: 'Amadeus: A Play', image: image, isFavorite: false },
+    { title: 'Annotated Alice: ...', image: image, isFavorite: false },
+    { title: 'Applied Numeric.', image: image, isFavorite: false },
+    { title: 'Artaud Anthology', image: image, isFavorite: false },
+    { title: 'Asterios Polyp', image: image, isFavorite: false },
+    { title: 'Batman: Year One', image: image, isFavorite: false },
+    { title: 'The Great Gatsby', image: image, isFavorite: false },
+    { title: 'To Kill a Mockingbird', image: image, isFavorite: false },
+    { title: '1984', image: image, isFavorite: false },
+    { title: 'Moby-Dick', image: image, isFavorite: false },
     // Add more book data here
-  ];
+  ]);
+
+  const handleFavoriteClick = (index) => {
+    const updatedBooks = [...books];
+    updatedBooks[index].isFavorite = !updatedBooks[index].isFavorite;
+    setBooks(updatedBooks);
+  };
 
   return (
     <div>
-      <div className="flex items-center justify-between p-4">
-        <input
-          type="text"
-          placeholder="Start Searching..."
-          className="w-1/2 p-2 border rounded bg-white"
-        />
+      <div className="flex items-center justify-between p-2">
+        <div className="relative w-3/4 sm:w-1/2 p-2">
+          <input
+            type="text"
+            placeholder="Start Searching..."
+            className="pl-8 border rounded bg-white w-full h-10"
+          />
+          <SearchIcon className="absolute text-gray-500 left-3 top-4" />
+        </div>
         <div className="dropdown dropdown-end">
           <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
             <div className="w-10 rounded-full">
@@ -160,22 +176,25 @@ function Books() {
       </div>
 
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        {books.map((book) => (
+        {books.map((book, index) => (
           <div key={book.title} className="p-4">
             <img src={book.image} alt={book.title} className="w-full h-40 object-cover" />
             <div className="text-center mt-2 text-sm">{book.title}</div>
-            <div className="flex w-full">
-            <button
-              type="button"
-              className="mt-2 w-full mr-2 inline-block rounded border-2 border-neutral-800 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-800 transition duration-150 ease-in-out hover:border-neutral-800 hover:bg-neutral-100 hover:text-white focus:border-neutral-800 focus:bg-neutral-100 focus:text-white focus:ring-0 active:border-neutral-900 active:text-neutral-900 motion-reduce:transition-none dark:text-neutral-600 dark:hover:bg-neutral-900 dark:focus:bg-neutral-900"
-              data-twe-ripple-init
-            >
-              Borrow
-            </button>
-            {/* <svg xmlns="http://www.w3.org/2000/svg" className="mt-3.5" fill="red" style={{cursor:"pointer"}} width="24" height="24" viewBox="0 0 24 24"><path d="M12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412l7.332 7.332c.17.299.498.492.875.492a.99.99 0 0 0 .792-.409l7.415-7.415c2.354-2.354 2.354-6.049-.002-8.416a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595zm6.791 1.61c1.563 1.571 1.564 4.025.002 5.588L12 18.586l-6.793-6.793c-1.562-1.563-1.561-4.017-.002-5.584.76-.756 1.754-1.172 2.799-1.172s2.035.416 2.789 1.17l.5.5a.999.999 0 0 0 1.414 0l.5-.5c1.512-1.509 4.074-1.505 5.584-.002z"/></svg> */}
-            {/* <svg xmlns="http://www.w3.org/2000/svg" className="mt-3.5" fill="red" style={{cursor:"pointer"}} width="24" height="24" viewBox="0 0 24 24"><path d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z"/></svg> */}
+            <div>
+              <button
+                type="button"
+                className="mt-2 w-4/5 inline-block rounded border-2 border-neutral-800 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-800 transition duration-150 ease-in-out hover:border-neutral-800 hover:bg-neutral-100 hover:text-white focus:border-neutral-800 focus:bg-neutral-100 focus:text-white focus:ring-0 active:border-neutral-900 active:text-neutral-900 motion-reduce:transition-none dark:text-neutral-600 dark:hover:bg-neutral-900 dark:focus:bg-neutral-900"
+                data-twe-ripple-init
+              >
+                Borrow
+              </button>
+              <FavoriteIcon
+                className={`ml-2 stroke-black stroke-2 cursor-pointer ${book.isFavorite ? 'text-pink-500 stroke-none' : 'text-white'}`}
+                onClick={() => handleFavoriteClick(index)}
+                style={{ transition: 'color 0.3s' }} // Smooth transition for color change
+              />
             </div>
-            
+
           </div>
         ))}
       </div>
@@ -217,7 +236,7 @@ function Books() {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
               <path
                 fillRule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 0 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd" />
             </svg>
           </a>
         </div>
@@ -226,67 +245,68 @@ function Books() {
   );
 }
 
-function Dashboard() {
-  // Sample book data
-  const books = [
-    { title: "Book Title 1", imageUrl: image },
-    { title: "Book Title 2", imageUrl: image },
-    { title: "Book Title 3", imageUrl: image }
+function Home() {
+  const books = [{ title: "Book Title 1", imageUrl: image },
+  { title: "Book Title 2", imageUrl: image },
+  { title: "Book Title 3", imageUrl: image },
   ];
 
-  // Sample articles
   const articles = [
     { title: "Article Title 1", author: "Author 1", imageUrl: image },
-    { title: "Article Title 2", author: "Author 2", imageUrl: image }
+    { title: "Article Title 2", author: "Author 2", imageUrl: image },
   ];
 
   return (
     <div className="container mx-auto py-8">
       <div className="flex flex-wrap items-center mb-8">
         <div className="w-full md:w-1/2 lg:w-3/5 mb-4 md:mb-0">
-          <h1 className="text-3xl font-bold mb-4">Welcome to the library</h1>
-          <p className="mb-4">Explore our vast collection of books</p>
+          <h1 className="text-3xl font-bold mb-4 text-blue-500">Welcome to the library</h1>
+          <p className="mb-4 text-gray-600">Explore our vast collection of books</p>
         </div>
         <div className="w-full md:w-1/2 lg:w-2/5 px-2">
           <img className="w-1/2" src={image} alt="Logo" />
         </div>
       </div>
 
-      <h2 className="text-2xl font-bold mb-4">Featured Books</h2>
+      <h2 className="text-2xl font-bold mb-4 text-blue-500">Featured Books</h2>
       <div className="flex flex-wrap mb-8">
         {books.map((book, index) => (
           <div key={index} className="w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
-            <img className="w-full mb-3" src={book.imageUrl} alt="Featured Book" />
-            <p className="font-semibold text-center">{book.title}</p>
+            <img className="w-full mb-3" src={book.imageUrl} alt={book.title} />
+            <p className="font-semibold text-center text-gray-600">{book.title}</p>
           </div>
         ))}
       </div>
 
-      <h2 className="text-2xl font-bold mb-4">Popular Articles</h2>
+      <h2 className="text-2xl font-bold mb-4 text-blue-500">Popular Articles</h2>
       <div className="flex flex-wrap mb-8">
         {articles.map((article, index) => (
           <div key={index} className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4">
             <div className="flex items-center mb-2">
-              <img className="w-12 h-12 mr-2" src={article.imageUrl} alt="Article" />
+              <img className="w-12 h-12 mr-2" src={article.imageUrl} alt={article.title} />
               <div>
-                <p className="font-semibold">{article.title}</p>
-                <p>{article.author}</p>
+                <p className="font-semibold text-gray-600">{article.title}</p>
+                <p className="text-gray-500">{article.author}</p>
               </div>
             </div>
           </div>
         ))}
       </div>
 
-      <h2 className="text-2xl font-bold mb-2">Subscribe to Newsletter</h2>
-      <p className=" text-gray-400 mb-4">Receive the latest updates and offers directly to your inbox!</p>
+      <h2 className="text-2xl font-bold mb-2 text-blue-500">Subscribe to Newsletter</h2>
+      <p className="text-gray-400 mb-4">Receive the latest updates and offers directly to your inbox!</p>
       <div className="flex items-center mb-2">
-        <input type="email" className="px-4 py-2 rounded-md w-5/12 border border-gray-600 bg-white" placeholder="Enter your email" />
+        <input
+          type="email"
+          className="px-4 py-2 rounded-md w-5/12 border bg-white border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          placeholder="Enter your email"
+        />
       </div>
       <button className="px-6 py-2 bg-blue-500 text-white hover:bg-blue-600 rounded-md mb-8">Subscribe</button>
 
-      <h2 className="text-2xl font-bold mb-4">Library Statistics</h2>
+      <h2 className="text-2xl font-bold mb-4 text-blue-500">Library Statistics</h2>
       <div className="flex flex-wrap items-center mb-8">
-        <div className="flex justify-center space-x-5  md:w-1/2 lg:w-1/3 px-2 mb-4">
+        <div className="w-full md:w-1/2 lg:w-1/3 px-2 mb-4 text-gray-600">
           <p>Total Books: 500+</p>
           <p>Total Active Members: 200+</p>
         </div>
@@ -383,50 +403,35 @@ function Dashboard() {
         </div>
       </div>
 
-      <h2 className="text-2xl font-bold mb-4">Location of Library</h2>
-      <p>Library Address</p>
+      <h2 className="text-2xl font-bold mb-4 text-blue-500">Location of Library</h2>
+      <p className="text-gray-600">Library Address</p>
 
       <div className="flex justify-center space-x-4 mt-8">
-        <p>Contact Us</p>
-        <p>About</p>
-        <p>Terms and Conditions</p>
+        <InstagramIcon className="hover:text-pink-500 text-2xl" />
+        <XIcon className="hover:text-black text-2xl" />
+        <LinkedInIcon className="hover:text-blue-600 text-2xl" />
       </div>
     </div>
   );
-}
+};
 
-function Account() {
-  return (
-    <div>
-      <h1>Account</h1>
 
-    </div>
-  );
-}
+// function HomeIcon(props) {
+//   return (
+//     <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="none" viewBox="0 0 19 19" {...props}>
+//       <path fill="currentColor" d="M10.216.018v6h8v-6m-8 18h8v-10h-8m-10 10h8v-6h-8v10Z" />
+//     </svg>
+//   );
+// }
 
-function Status() {
-  return (
-    <div>
-      <h1>Status</h1>
-    </div>
-  );
-}
 
-function DashboardIcon(props) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="none" viewBox="0 0 19 19" {...props}>
-      <path fill="currentColor" d="M10.216.018v6h8v-6m-8 18h8v-10h-8m-10 10h8v-6h-8v10Z" />
-    </svg>
-  );
-}
-
-function BooksIcon(props) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="37" fill="none" viewBox="0 0 36 37" stroke="currentColor" strokeWidth="2" {...props}>
-      <path strokeLinecap="round" fill="currentColor" strokeLinejoin="round" stroke="white" d="M17.1 6.878c-1.37-.909-3.422-1.462-5.85-1.462-4.433 0-7.65 1.823-7.65 4.336v20.864a.9.9 0 1 0 1.8 0c0-1.277 2.403-2.7 5.85-2.7 1.9 0 3.483.432 4.521 1.034.591.343 1.329-.104 1.329-.788V6.878Zm7.65-1.462c-2.428 0-4.48.553-5.85 1.462v21.284c0 .684.738 1.13 1.329.788 1.038-.602 2.622-1.034 4.521-1.034 3.447 0 5.85 1.423 5.85 2.7a.9.9 0 1 0 1.8 0V9.752c0-2.513-3.217-4.336-7.65-4.336Z" />
-    </svg>
-  );
-}
+// function BooksIcon(props) {
+//   return (
+//     <svg xmlns="http://www.w3.org/2000/svg" width="36" height="37" fill="none" viewBox="0 0 36 37" stroke="currentColor" strokeWidth="2" {...props}>
+//       <path strokeLinecap="round" fill="currentColor" strokeLinejoin="round" stroke="white" d="M17.1 6.878c-1.37-.909-3.422-1.462-5.85-1.462-4.433 0-7.65 1.823-7.65 4.336v20.864a.9.9 0 1 0 1.8 0c0-1.277 2.403-2.7 5.85-2.7 1.9 0 3.483.432 4.521 1.034.591.343 1.329-.104 1.329-.788V6.878Zm7.65-1.462c-2.428 0-4.48.553-5.85 1.462v21.284c0 .684.738 1.13 1.329.788 1.038-.602 2.622-1.034 4.521-1.034 3.447 0 5.85 1.423 5.85 2.7a.9.9 0 1 0 1.8 0V9.752c0-2.513-3.217-4.336-7.65-4.336Z" />
+//     </svg>
+//   );
+// }
 
 function StatusIcon(props) {
   return (
@@ -436,25 +441,25 @@ function StatusIcon(props) {
   );
 }
 
-function AccountIcon(props) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      className="size-5 opacity-75"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      strokeWidth="2"
-      {...props}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        fill="currentColor"
-        d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-      />
-    </svg>
-  );
-}
+// function AccountIcon(props) {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       className="size-5 opacity-75"
+//       fill="none"
+//       viewBox="0 0 24 24"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       {...props}
+//     >
+//       <path
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//         fill="currentColor"
+//         d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+//       />
+//     </svg>
+//   );
+// }
 
 export default App;
