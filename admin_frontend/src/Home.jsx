fbfd613166b190d7a1d2bc0d2863592c07861cda
KaranK369 <KaranK369@users.noreply.github.com>
2024-06-18 01:39:28 +0530

    admin changes

--- admin_frontend/src/Home.jsx
+++ admin_frontend/src/Home.jsx
@@ -2,39 +2,51 @@ import React from "react";
 
 function Home() {
   return (
-    <section class="text-gray-900 body-font">
+    <section class=" text-gray-900 body-font">
   <div class="container px-5 py-10 mx-auto">
-    <div class="flex flex-wrap w-full mb-20">
-      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">Pitchfork Kickstarter Taxidermy</h1>
-        <div class="h-1 w-20 bg-indigo-500 rounded"></div>
-      </div>
-      <h1 class="lg:w-1/2 w-full leading-relaxed text-black">time jun | 9:00</h1>
+    <div class="flex flex-col text-left w-full mb-20">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Master</h1>
     </div>
-    <div class="flex flex-wrap -m-4">
-      <div class="xl:w-1/4 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <h1 class=" text-4xl text-black font-bold mb-4">300</h1>
-          <p class="leading-relaxed text-base">User List</p>
+    <div class="flex flex-wrap -m-4 text-center">
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-2  bg-gray-100 border-gray-300 px-4 py-6 rounded-lg">
+        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-10 h-8 mb-2 inline-block" viewBox="0 0 24 24">
+            <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
+            <circle cx="9" cy="7" r="4"></circle>
+            <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
+          </svg>
+          <h2 class="title-font font-semibold text-4xl text-gray-900">2.7K</h2>
+          <p class="leading-relaxed">Total Visitors</p>
         </div>
       </div>
-      <div class="xl:w-1/4 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <h1 class="text-4xl text-black font-bold title-font mb-4">300</h1>
-          <p class="leading-relaxed text-base">Overdue</p>
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-2 bg-gray-100 border-gray-300 px-4 py-6 rounded-lg">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-10 h-8 mb-2 inline-block" viewBox="0 0 24 24">
+            <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
+            <circle cx="9" cy="7" r="4"></circle>
+            <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
+          </svg>
+          <h2 class="title-font font-semibold text-4xl text-gray-900">1.3K</h2>
+          <p class="leading-relaxed">Borrowed Books</p>
         </div>
       </div>
-      <div class="xl:w-1/4 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          <h1 class="text-4xl text-black font-bold title-font mb-4">30</h1>
-          <p class="leading-relaxed text-base">Book List</p>
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-2  bg-gray-100 border-gray-300 px-4 py-6 rounded-lg">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-10 h-8 mb-2 inline-block" viewBox="0 0 24 24">
+            <path d="M3 18v-6a9 9 0 0118 0v6"></path>
+            <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
+          </svg>
+          <h2 class="title-font font-semibold text-4xl text-gray-900">74</h2>
+          <p class="leading-relaxed">Overdue Books</p>
         </div>
       </div>
-      <div class="xl:w-1/4 md:w-1/2 p-4">
-        <div class="bg-gray-100 p-6 rounded-lg">
-          
-          <h1 class="text-4xl text-black font-bold title-font mb-4">300</h1>
-          <p class="leading-relaxed text-base">Pending</p>
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-2  bg-gray-100 border-gray-300 px-4 py-6 rounded-lg">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 w-10 h-8 mb-2 inline-block" viewBox="0 0 24 24">
+            <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+          </svg>
+          <h2 class="title-font font-semibold text-4xl text-gray-900">46</h2>
+          <p class="leading-relaxed">Book Request</p>
         </div>
       </div>
     </div>
@@ -42,5 +54,6 @@ function Home() {
 </section>
   );
 }
+
 
 export default Home;
