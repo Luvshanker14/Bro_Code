1a3b61dfe14a5a03dab0843d04e74c2ee3380cef
KaranK369 <KaranK369@users.noreply.github.com>
2024-06-18 18:49:22 +0530

    added time in admin_frontend

---
+++ admin_frontend/src/Home_components/Time.jsx
@@ -0,0 +1,47 @@
+import React,{ useState, useEffect } from "react";
+
+function Time() {
+    const [currentTime, setCurrentTime] = useState(new Date());
+  
+    useEffect(() => {
+      const intervalId = setInterval(() => {
+        setCurrentTime(new Date());
+      }, 1000);
+  
+      return () => clearInterval(intervalId);
+    }, []);
+  
+    const formatDate = (date) => {
+      const options = { year: 'numeric', month: 'short', day: 'numeric' };
+      return date.toLocaleDateString(undefined, options);
+    };
+  
+    const formatDay = (date) => {
+      const options = { weekday: 'long' };
+      return date.toLocaleDateString(undefined, options);
+    };
+  
+    const formatTime = (date) => {
+      const options = { hour: '2-digit',minute:'2-digit' };
+      return date.toLocaleTimeString(undefined, options);
+    };
+  
+    return (
+      <div className="flex text-left">
+        <h1 className="font-mono text-2.5xl text-gray-900">
+          {formatDate(currentTime)}
+        </h1>
+        <h1 className="pl-4 text-2xl font-bold text-gray-900">
+          |
+        </h1>
+        <h2 className="font-mono text-2.5xl pl-3 pr-5 text-gray-900">
+          {formatDay(currentTime)}
+        </h2>
+        <h2 className="font-mono text-2.5xl text-gray-900">
+          {formatTime(currentTime)}
+        </h2>
+      </div>
+    );
+  }
+
+  export default Time;
--- admin_frontend/src/Navbar.jsx
+++ admin_frontend/src/Navbar.jsx
@@ -11,51 +11,51 @@ function Navbar({ selected, setSelected }) {
   ];
 
   return (
-    <div className="navbar-container">
-      <div>
-        <div className="navbar-logo">
-          <span>
-            <img src="iitdh_logo.png" />
-          </span>
-        </div>
-        <div className="navbar-divider">
-          <div className="px-2">
-            <div className="py-4">
-              {menuItems.map((item) => (
-                <MenuItem
-                  key={item.name}
-                  name={item.name}
-                  icon={item.icon}
-                  selected={selected}
-                  setSelected={setSelected}
-                />
-              ))}
+      <div className="navbar-container">
+        <div>
+          <div className="navbar-logo">
+            <span>
+              <img src="iitdh_logo.png" />
+            </span>
+          </div>
+          <div className="navbar-divider">
+            <div className="px-2">
+              <div className="py-4">
+                {menuItems.map((item) => (
+                  <MenuItem
+                    key={item.name}
+                    name={item.name}
+                    icon={item.icon}
+                    selected={selected}
+                    setSelected={setSelected}
+                  />
+                ))}
+              </div>
             </div>
           </div>
         </div>
+        <div className="logout-button">
+          <form action="#">
+            <button type="submit" className="logout-button">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-5 opacity-75"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+              <span>Logout</span>
+            </button>
+          </form>
+        </div>
       </div>
-      <div className="logout-button">
-        <form action="#">
-          <button type="submit" className="logout-button">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="size-5 opacity-75"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-              />
-            </svg>
-            <span>Logout</span>
-          </button>
-        </form>
-      </div>
-    </div>
   );
 }
 
