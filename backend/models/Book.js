b3b173298e517bfedb12f27ff032e71b6580fbfb
Vishal Kumar <vishalkumar1981v@gmail.com>
2024-06-27 13:22:23 +0530

    API to fetch data from database

---
+++ backend/models/Book.js
@@ -0,0 +1,19 @@
+// models/Book.js
+const mongoose = require('mongoose');
+
+const bookSchema = mongoose.Schema({
+    title: String,
+    description: String,
+    author: String,
+    genre: String,
+    department: String,
+    count: Number,
+    vendor: String,
+    vendor_id: Number,
+    publisher: String,
+    publisher_id: Number
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
