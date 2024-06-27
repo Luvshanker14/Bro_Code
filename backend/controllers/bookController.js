47282cc038b69fbf762fa8f209ddf209e8187199
Vishal Kumar <vishalkumar1981v@gmail.com>
2024-06-27 13:22:23 +0530

    API to fetch data from database

---
+++ backend/controllers/bookController.js
@@ -0,0 +1,12 @@
+// controllers/bookController.js
+const Book = require('../models/Book');
+
+exports.getBooks = async (req, res) => {
+    try {
+        const books = await Book.find();
+        res.status(200).json(books);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,28 @@
-const express=require("express");
-const mongoose=require("mongoose");
-const bodyParser=require("body-parser");
-require("dotenv").config();
-const cors=require('cors');
+// index.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const bookRoutes = require('./routes/bookRoutes');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const app=express();
-//app.use(cors());
-
-const corsOptions = {
-  origin: 'http://127.0.0.1:3000',
-  optionsSuccessStatus: 200 // Some legacy browsers choke on 204
-};
-
-app.use(cors(corsOptions));
-
-
+// Middleware
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
 
-const PORT =process.env.PORT || 4000;
-
-app.get('/',(req,res)=>{
-res.json({msg:"Welcome To LMS Server"});
-})
-
-app.use('/user',require('./routes/userRouter'));
-
-
-
-
-
-
-
-app.listen(PORT,()=>{
-  console.log(`Server is running at ${PORT}`);
-})
-
-app.use((error,req,res,next)=>{
-  res.status(500).send("Oops! Something is up with our Server")
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/booksdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Could not connect to MongoDB', err);
 });
 
-const URL=process.env.MONGODB_URL;
-mongoose.connect(URL,{
-  useNewUrlParser:true,
-  useUnifiedTopology:true
+// Routes
+app.use('/books', bookRoutes);
 
-}).then(()=>{
-  console.log("Database Connected");
-}).catch(err=>{
-  console.log(err);
-})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
