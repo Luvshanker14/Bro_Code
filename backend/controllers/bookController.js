143171a8d3d455235190f72dfc2358db0e6869f2
KaranK369 <KaranK369@users.noreply.github.com>
2024-07-11 15:28:37 +0530

    changes

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -132,3 +132,34 @@ exports.removeBookFromCart = async (req, res) => {
     res.status(500).json({ message: "An error occurred while removing the book from the cart", error: error.message });
   }
 };
+
+
+
+exports.getBookById = async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    res.status(200).json(book);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+
+exports.updateBookById = async (req, res) => {
+  try {
+    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!book) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+    res.status(200).json(book);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
