03259c65cf1a114d6a0137463bb1d0eaa4ab3037
KaranK369 <KaranK369@users.noreply.github.com>
2024-07-14 12:57:22 +0530

    added delete button

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,51 @@
 // index.js
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const bookRoutes = require('./routes/bookRoutes');
-const cors = require('cors');
-const bookRequestRoutes = require('./routes/bookRequestRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const bookRoutes = require("./routes/bookRoutes");
+const cors = require("cors");
+const bookRequestRoutes = require("./routes/bookRequestRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const dotenv = require('dotenv');
-const userRoute = require('./routes/userRouter');
-const adminRoute=require('./routes/adminRoute');
-const imageRoute=require('./routes/imageRoutes');
-const mailRoute = require('./routes/mailRoute')
-const path = require('path');
+const dotenv = require("dotenv");
+const userRoute = require("./routes/userRouter");
+const adminRoute = require("./routes/adminRoute");
+const imageRoute = require("./routes/imageRoutes");
+const mailRoute = require("./routes/mailRoute");
+const path = require("path");
 dotenv.config();
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 // MongoDB connection
-const URL=process.env.MONGODB_URL;
-mongoose.connect(URL, {
+const URL = process.env.MONGODB_URL;
+mongoose
+  .connect(URL, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error('Could not connect to MongoDB', err);
-});
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB", err);
+  });
 
 // Routes
-app.get('/', (req, res) => {
-    res.json({ msg: "Welcome To LMS Server" });
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome To LMS Server" });
+});
 
-app.use('/user', userRoute);
-app.use('/books', bookRoutes);
-app.use('/bookRequests', bookRequestRoutes);
-app.use('/admin',adminRoute);
-app.use('/image',imageRoute);
-app.use('/uploads', express.static('uploads'));
+app.use("/user", userRoute);
+app.use("/books", bookRoutes);
+app.use("/bookRequests", bookRequestRoutes);
+app.use("/admin", adminRoute);
+app.use("/image", imageRoute);
+app.use("/uploads", express.static("uploads"));
 app.use("/mail", mailRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
